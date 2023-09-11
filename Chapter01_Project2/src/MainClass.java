c25437e6881d489c204ea266cb35f76ce5a68eee
parkh <parkh@DESKTOP-AR0MS6Q>
2023-09-11 17:45:27 +0900

    commit

--- Chapter01_Project2/src/MainClass.java
+++ Chapter01_Project2/src/MainClass.java
@@ -1,9 +1,34 @@
-
+/*
+ * 	프로그램
+ * 		1. 데이터 저장
+ * 			변수 : 변하는 수 (필요한 경우 변경이 가능)
+ * 			상수 : 변하지 않는 수
+ * 			
+ * 			저장해야 되는 데이터가 많은 경우
+ * 				묶어서 저장 => 배열/클래스(사용자 정의)
+ * 			ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ 메모리에 저장(RAM), 휘발성
+ * 			사라지지 않는 저장값 : 파일, Database
+ * 		2. 데이터 가공(사용자 요청)
+ * 			연산자, 제어문 : 메소드
+ * 			C/C++은 함수, Java/C#은 메소드 : 클래스 종속의 차이
+ * 			함수는 독립적, 메소드는 클래스에 종속
+ * 		3. 가공된 데이터 출력
+ * 			Application(Window, dos) => main
+ * 			Web Application(브라우저) => main(X)
+ * 		ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ 관련된 내용을 묶어서 관리
+ * 		4. 자바를 배우는 이유
+ * 			데이터 관리, 브라우저 전송, 오라클 연동
+ * 		5. 라이브러리
+ * 			java.util, java.io, java.net
+ * 			mvnrepository.com => 라이브러리 집합소
+ * 		6. 조립법
+ * 		7. 알고리즘, 디자인 패턴
+ */
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
