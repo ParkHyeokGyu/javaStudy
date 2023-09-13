b450209db796b6f758328b807f201ff3bd65322b
parkh <parkh@DESKTOP-AR0MS6Q>
2023-09-13 17:49:48 +0900

    09/13 연산자문제

--- Chapter02_연산자문제/src/문제12.java
+++ Chapter02_연산자문제/src/문제12.java
@@ -1,12 +1,23 @@
+import java.util.Scanner;
 
 public class 문제12 {
 	public static void main(String[] args) {
 		// 사용자로부터 두 개의 정수를 입력 받아서 뺄셈과 곱셈 연산의 결과를 출력하는 프로그램을 작성
-				int a=(int)(Math.random()*100)+1;
-				int b=(int)(Math.random()*100)+1;
+		Scanner sc=new Scanner(System.in);
+//		int a=(int)(Math.random()*100)+1;
+//		int b=(int)(Math.random()*100)+1;
+		
+//		System.out.print("첫번쨰 정수 입력:");
+//		int a=sc.nextInt();
+//		System.out.print("두번쨰 정수 입력:");
+//		int b=sc.nextInt();
+		
+		System.out.print("정수 두개 입력(10 20):");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
 				
-				System.out.println("a:"+a+",b:"+b);
-				System.out.println("a-b:"+(a-b));
-				System.out.println("a*b:"+(a*b));
+		System.out.println("a:"+a+",b:"+b);
+		System.out.println("a-b:"+(a-b));
+		System.out.println("a*b:"+(a*b));
 	}
 }
--- Chapter02_연산자문제/src/문제13.java
+++ Chapter02_연산자문제/src/문제13.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class 문제13 {
 	public static void main(String[] args) {
@@ -6,9 +7,15 @@ public class 문제13 {
 		 *	예를 들어 사용자로부터 순서대로 입력받은 세 개의 정수가 1, 2, 3 이라면 1 * 2 + 3의 계산 결과를 출혁해야 한다. 
 		 *	단 출력 양식은 다음과 같아야 한다.
 		 */
-		int a=(int)(Math.random()*100)+1;
-		int b=(int)(Math.random()*100)+1;
-		int c=(int)(Math.random()*100)+1;
+		Scanner sc=new Scanner(System.in);
+//		int a=(int)(Math.random()*100)+1;
+//		int b=(int)(Math.random()*100)+1;
+//		int c=(int)(Math.random()*100)+1;
+		
+		System.out.print("세개 정수 입력:");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
 		System.out.println("a:"+a+",b:"+b+",c:"+c);
 		System.out.println("a*b+c:"+(a*b+c));
 	}
