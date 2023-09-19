82a7d325cb473e3f5bc1f54ea5397264e0439ede
parkh <parkh@DESKTOP-AR0MS6Q>
2023-09-19 17:49:10 +0900

    Chapter03

--- Chapter03_제어문(반복문)_1/src/제어문_반복문2.java
+++ Chapter03_제어문(반복문)_1/src/제어문_반복문2.java
@@ -21,10 +21,17 @@
  */
 public class 제어문_반복문2 {
 	public static void main(String[] args) {
-		for(int i=1;i<=100;i+=2)
-			System.out.print(i+" ");
-		System.out.println();
-		for(int i=2;i<=100;i+=2)
-			System.out.print(i+" ");
+//		for(int i=1;i<=100;i+=2)
+//			System.out.print(i+" ");
+//		System.out.println();
+//		for(int i=2;i<=100;i+=2)
+//			System.out.print(i+" ");
+		
+		for(int i=1;i<=4;i++) { // 줄 수
+			for(int j=1;j<=i;j++) { // 실제 출력
+				System.out.print("★");
+			}
+			System.out.println();
+		}
 	}
 }
